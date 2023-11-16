fc91d05d91f5c26c0cc9b670348ec101bdec8b0a
ktaekim2 <ktaekim2@gmail.com>
2023-11-17 00:11:44 +0900

    [11021] A+B - 7

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello w" +
-                "orld!");
+    public static void main(String[] args) throws IOException {
+/*        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        System.out.println(n);
+        for(int i = 0 ; i <= n ; i++) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            System.out.println("Case #" + i + ": " + (a+b));
+        }
+        scanner.close();*/
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(bufferedReader.readLine());
+
+        StringTokenizer stringTokenizer;
+
+        for (int i = 1; i <= a; i++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine(), " ");
+            System.out.println("Case #" + i + ": " + (Integer.parseInt(stringTokenizer.nextToken()) + Integer.parseInt(stringTokenizer.nextToken())));
+        }
+
+        bufferedReader.close();
+
     }
 }
