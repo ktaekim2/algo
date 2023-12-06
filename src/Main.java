1c5f83f7b8fe9f5f5188dce627d7901bca2286cf
ktaekim2 <ktaekim2@gmail.com>
2023-12-06 20:20:20 +0900

    [10952] A+B - 5

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,23 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
-            int a = Integer.parseInt(bufferedReader.readLine());
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer;
 
-            String star = "*";
-            String blank = "";
-            for (int i = 1; i <= a; i++) {
-                for (int j = 0; j < a - i; j++) {
-                    blank += " ";
-                }
-                System.out.println(blank + star);
-                star += "*";
-                blank = "";
+        while (true) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine(), " ");
+
+            int a = Integer.parseInt(stringTokenizer.nextToken());
+            int b = Integer.parseInt(stringTokenizer.nextToken());
+            if (a == 0 && b == 0) {
+                break;
             }
-
-        } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println(a + b);
         }
+        bufferedReader.close();
     }
 }
