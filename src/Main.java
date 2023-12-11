f232e698f59ba74883f71e3e529f0123e42bbac4
ktaekim2 <ktaekim2@gmail.com>
2023-12-11 12:49:27 +0900

    [10807] 갯수 세기

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,35 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
-        String str;
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        while((str = br.readLine()) != null) {
-            st = new StringTokenizer(str, " ");
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            sb.append(a + b).append("\n");
+        int n = Integer.parseInt(br.readLine());
+        int[] array = new int[n];
+
+        String inputLine = br.readLine();
+        StringTokenizer st = new StringTokenizer(inputLine, " ");
+
+        for (int i = 0; i < n; i++) {
+            if(st.hasMoreTokens()) {
+                array[i] = Integer.parseInt(st.nextToken());
+            }
         }
-        System.out.println(sb);
+
+        int c = Integer.parseInt(br.readLine());
+        int count = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if(array[i] == c) {
+                count++;
+            }
+        }
+        bw.write(String.valueOf(count));
+
+        br.close();
+        bw.flush();
+        bw.close();
     }
 }
