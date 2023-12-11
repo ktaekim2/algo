0084584686d7051573a23fe500fb00ddb154f406
ktaekim2 <ktaekim2@gmail.com>
2023-12-11 12:54:53 +0900

    [10807] 갯수 세기 - try-with-resources

--- src/Main.java
+++ src/Main.java
@@ -3,33 +3,33 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));) {
 
-        int n = Integer.parseInt(br.readLine());
-        int[] array = new int[n];
+            int n = Integer.parseInt(br.readLine());
+            int[] array = new int[n];
 
-        String inputLine = br.readLine();
-        StringTokenizer st = new StringTokenizer(inputLine, " ");
+            String inputLine = br.readLine();
+            StringTokenizer st = new StringTokenizer(inputLine, " ");
 
-        for (int i = 0; i < n; i++) {
-            if(st.hasMoreTokens()) {
-                array[i] = Integer.parseInt(st.nextToken());
+            for (int i = 0; i < n; i++) {
+                if (st.hasMoreTokens()) {
+                    array[i] = Integer.parseInt(st.nextToken());
+                }
             }
-        }
 
-        int c = Integer.parseInt(br.readLine());
-        int count = 0;
+            int c = Integer.parseInt(br.readLine());
+            int count = 0;
 
-        for (int i = 0; i < array.length; i++) {
-            if(array[i] == c) {
-                count++;
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] == c) {
+                    count++;
+                }
             }
+            bw.write(String.valueOf(count));
+            bw.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        bw.write(String.valueOf(count));
-
-        br.close();
-        bw.flush();
-        bw.close();
     }
 }
