87c29fe6cddba13b773175b717e91763204c22cd
ktaekim2 <ktaekim2@gmail.com>
2023-12-12 12:44:27 +0900

    [10871] X보다 작은 수

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,20 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
              BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));) {
 
-            int n = Integer.parseInt(br.readLine());
-            int[] array = new int[n];
-
             String inputLine = br.readLine();
             StringTokenizer st = new StringTokenizer(inputLine, " ");
+
+            int n = Integer.parseInt(st.nextToken());
+
+            int[] array = new int[n];
+            int x = Integer.parseInt(st.nextToken());
+
+            String ar = br.readLine();
+            st = new StringTokenizer(ar, " ");
 
             for (int i = 0; i < n; i++) {
                 if (st.hasMoreTokens()) {
@@ -18,15 +23,15 @@ public class Main {
                 }
             }
 
-            int c = Integer.parseInt(br.readLine());
-            int count = 0;
+            String result = "";
 
             for (int i = 0; i < array.length; i++) {
-                if (array[i] == c) {
-                    count++;
+                if (array[i] < x) {
+                    result += String.valueOf(array[i] + " ");
                 }
             }
-            bw.write(String.valueOf(count));
+
+            bw.write(result);
             bw.flush();
         } catch (IOException e) {
             e.printStackTrace();
