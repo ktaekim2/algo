ec2ea5ca280db03082b496a2fb49c6fa5674411f
ktaekim2 <ktaekim2@gmail.com>
2024-01-11 12:45:46 +0900

    [10818] 최소, 최대

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,70 @@
+import com.sun.java.swing.plaf.windows.WindowsTabbedPaneUI;
+import sun.util.BuddhistCalendar;
+
 import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        // 배열X
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int max = -1000001;
+        int min = 1000001;
+
+        while(st.hasMoreTokens()) {
+            int val = Integer.parseInt(st.nextToken());
+            if(val > max) {
+                max = val;
+            }
+            if(val < min) {
+                min = val;
+            }
+        }
+        System.out.println(min + " " + max);
+
+/*
+        // 버퍼리더 sort 사용
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int index = 0;
+        int[] arr = new int[N];
+        while(st.hasMoreTokens()) {
+            arr[index] = Integer.parseInt(st.nextToken());
+            index++;
+        }
+
+        Arrays.sort(arr);
+        System.out.println(arr[0] + " " + arr[N - 1]);
+*/
+
+/*
+        // 스캐너 사용
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        in.close();
+        Arrays.sort(arr);
+        System.out.println(arr[0] + " " + arr[N - 1]  );
+*/
+
+/*
+        // 버퍼리더 자작
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
              BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));) {
 
@@ -12,7 +74,6 @@ public class Main {
             int n = Integer.parseInt(st.nextToken());
 
             int[] array = new int[n];
-            int x = Integer.parseInt(st.nextToken());
 
             String ar = br.readLine();
             st = new StringTokenizer(ar, " ");
@@ -23,18 +84,36 @@ public class Main {
                 }
             }
 
-            String result = "";
+            int max = 0;
+            int min = 0;
 
             for (int i = 0; i < array.length; i++) {
-                if (array[i] < x) {
-                    result += String.valueOf(array[i] + " ");
+                int compIdx = array[i];
+                if(i == 0) {
+                    max = compIdx;
+                    min = compIdx;
+                    continue;
+                }
+                if(compIdx > max) {
+                    max = compIdx;
+                }
+
+                if(compIdx < min) {
+                    min = compIdx;
                 }
             }
+
+            String result = "";
+
+            result = min + " " + max;
 
             bw.write(result);
             bw.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
+*/
+
+
     }
 }
